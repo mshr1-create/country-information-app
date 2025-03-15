b62aaf791b9070c82b4c27a919a1ca8b20d7f0e4
mshr1-create <143092368+mshr1-create@users.noreply.github.com>
2025-03-15 17:45:21 +0900

    Add SerchForm.js & App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchForm from './components/SearchForm';
 
 function App() {
+  const handleSearch = (searchValue) => {
+    console.log("検索された国名:", searchValue);
+    // ここに REST Countries API 呼び出しの処理等を追加する
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>国情報取得アプリ</h2>
+        <SearchForm onSearch={handleSearch} />
       </header>
     </div>
   );
